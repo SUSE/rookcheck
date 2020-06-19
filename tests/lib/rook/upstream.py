71fcc02fcc71dc3f51fed010a5d02604bfb096b8
Eric Jackson <swiftgist@gmail.com>
2020-06-19 16:13:03 -0400

    Build rook in workspace_dir
    Set TMPDIR to use the workspace_dir setting to consolidate temporary
    files
    
    Signed-off-by: Eric Jackson <swiftgist@gmail.com>

--- tests/lib/rook/upstream.py
+++ tests/lib/rook/upstream.py
@@ -18,6 +18,7 @@ import re
 import time
 import wget
 
+from tests.config import settings
 from tests.lib.common import execute
 from tests.lib import common
 from tests.lib.rook.base import RookBase
@@ -67,9 +68,11 @@ class RookCluster(RookBase):
         logger.info("[build_rook] Make rook")
         execute(
             "PATH={builddir}/go/bin:$PATH GOPATH={builddir} "
+            "TMPDIR={workspace_dir} "
             "make --directory='{builddir}/src/github.com/rook/rook' "
             "-j BUILD_REGISTRY='rook-build' IMAGES='ceph' "
-            "build".format(builddir=self.builddir),
+            "build".format(builddir=self.builddir,
+                           workspace_dir=settings.WORKSPACE_DIR),
             log_stderr=False,
             logger_name="make -j BUILD_REGISTRY='rook-build' IMAGES='ceph'",
         )
