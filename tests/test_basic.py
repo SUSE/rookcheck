c7380165bbeb52478bd36b6c5b86a97bba6756f2
Joshua Hesketh <josh@nitrotech.org>
2020-08-06 22:12:51 +1000

    Merge test_services and test_basic
    To reduce the number of deployments.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -16,6 +16,7 @@ import logging
 import os
 import pytest
 import time
+import yaml
 
 from tests.lib.hardware.node_base import NodeRole
 
@@ -105,6 +106,65 @@ def test_file_creation(rook_cluster):
         "delete deployment.apps/rook-direct-mount -n rook-ceph")
 
     logger.debug("Test successful/complete!")
+
+
+# check if rbd service gets started automatically
+def test_service_mons(rook_cluster):
+    cluster_yaml = os.path.join(rook_cluster.ceph_dir, 'cluster.yaml')
+    with open(cluster_yaml, 'r') as f:
+        cluster_object = yaml.load(f, Loader=yaml.BaseLoader)
+    # get configured number of monitors
+    count = cluster_object['spec']['mon']['count']
+    count = int(count)
+    logger.info("Expecting %d monitors", count)
+
+    # get number of mon-services
+    services = rook_cluster.kubernetes.get_services_by_app_label(
+        "rook-ceph-mon")
+
+    if count != len(services):
+        pytest.fail("Expected %d mon-services but got %d",
+                    count, len(services))
+
+    # get number of mon-pods
+    pods = rook_cluster.kubernetes.get_pods_by_app_label("rook-ceph-mon")
+    if count != len(pods):
+        pytest.fail("Expected %d mon-pods but got %d",
+                    count, len(pods))
+
+
+# check if all default services are available
+def test_services(rook_cluster):
+    services = ["csi-cephfsplugin-metrics",
+                "csi-rbdplugin-metrics",
+                "rook-ceph-mgr",
+                "rook-ceph-mgr-dashboard",
+                "rook-ceph-mon-a",
+                "rook-ceph-mon-b",
+                "rook-ceph-mon-c"]
+
+    for service in services:
+        found = rook_cluster.kubernetes.wait_for_service(service)
+        if found is False:
+            pytest.fail("Could not find service %s", service)
+
+
+# check if rbd service gets started automatically
+def test_service_rbd(rook_cluster):
+    # create an ObjectStore
+    output = rook_cluster.kubernetes.kubectl_apply(
+                os.path.join(rook_cluster.ceph_dir, 'object-test.yaml'))
+
+    if output[0] != 0:
+        pytest.fail("Could not create an ObjectStore")
+
+    found = rook_cluster.kubernetes.wait_for_service("rook-ceph-rgw-my-store",
+                                                     iteration=20)
+
+    if found is False:
+        pytest.fail("rgw service has not been started automatically")
+
+    # NOTE(jhesketh): This test leaves the rgw running in the cluster.
 
 
 def test_osd_number(rook_cluster):
