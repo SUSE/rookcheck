5fac5cc605f7f602b4e43d8f68cb55eb25abcd5b
Joshua Hesketh <josh@nitrotech.org>
2020-08-06 22:10:39 +1000

    Merge test_check_num_osd into test_basic
    To reduce the number of deployments needed.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -14,7 +14,10 @@
 
 import logging
 import os
+import pytest
 import time
+
+from tests.lib.hardware.node_base import NodeRole
 
 
 logger = logging.getLogger(__name__)
@@ -102,3 +105,57 @@ def test_file_creation(rook_cluster):
         "delete deployment.apps/rook-direct-mount -n rook-ceph")
 
     logger.debug("Test successful/complete!")
+
+
+def test_osd_number(rook_cluster):
+    # get number of workers
+    workers = len(rook_cluster.kubernetes.hardware.workers)
+    logger.debug("cluster has %s worker nodes", workers)
+
+    osds = rook_cluster.get_number_of_osds()
+    i = 0
+    while osds != workers:
+        if i == 20:
+            pytest.fail("rook did not add an additional osd-node")
+            break
+        time.sleep(10)
+        osds = rook_cluster.get_number_of_osds()
+        i += 1
+
+
+def test_add_node(rook_cluster):
+    workers_old = len(rook_cluster.kubernetes.hardware.workers)
+    # add a node to the cluster
+    node_name = "%s_worker_%s" % (rook_cluster.workspace.name, "test-node")
+    node = rook_cluster.kubernetes.hardware.node_create(node_name,
+                                                        NodeRole.WORKER,
+                                                        ["worker"])
+    # add a disk of 10 G the node
+    node.disk_create(10)
+    rook_cluster.kubernetes.hardware.node_add(node)
+    rook_cluster.kubernetes.hardware.prepare_nodes()
+    # add the node the k8s cluster
+    rook_cluster.kubernetes.join([node])
+
+    # get number of new workers
+    workers_new = len(rook_cluster.kubernetes.hardware.workers)
+    i = 0
+    while workers_new == workers_old:
+        if i == 10:
+            pytest.fail("Was not able to add an additional node")
+            break
+        time.sleep(10)
+        workers_new = len(rook_cluster.kubernetes.hardware.workers)
+        i += 1
+
+    # get number of new osds
+    osds = rook_cluster.get_number_of_osds()
+
+    i = 0
+    while osds != workers_new:
+        if i == 20:
+            pytest.fail("rook did not add an additional osd-node")
+            break
+        time.sleep(10)
+        osds = rook_cluster.get_number_of_osds()
+        i += 1
